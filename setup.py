a6a19c4a537214d228ed083ac5ef5ff5881e0786
Denis Duarte <den.duarte@gmail.com>
2021-04-18 14:59:01 -0300

    realease 0.6

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='workplace_extractor',
     packages=['workplace_extractor'],
-    version='0.5',
+    version='0.6',
     license='MIT',
     description='Extract posts created in a corporate Workplace by Facebook installation using the Graph API',
     author='Denis Duarte',
     author_email='den.duarte@gmail.com',
     url='https://github.com/denisduarte/workplace_extractor',
-    download_url='https://github.com/denisduarte/workplace_extractor/archive/refs/tags/v_05.tar.gz',
+    download_url='https://github.com/denisduarte/workplace_extractor/archive/refs/tags/v_06.tar.gz',
     keywords=['extraction', 'posts', 'workplace'],
     install_requires=[
         'numpy',
