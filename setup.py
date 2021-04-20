d70721de27f02612a408597359e63ffd626619fe
Denis Duarte <den.duarte@gmail.com>
2021-04-20 18:13:57 -0300

    Adding columns message and story

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='workplace_extractor',
     packages=['workplace_extractor', 'workplace_extractor.Nodes', 'workplace_extractor.Extractors'],
-    version='0.6.6',
+    version='0.6.7',
     license='MIT',
     description='Extract posts created in a corporate Workplace by Facebook installation using the Graph API',
     author='Denis Duarte',
     author_email='den.duarte@gmail.com',
     url='https://github.com/denisduarte/workplace_extractor',
-    download_url='https://github.com/denisduarte/workplace_extractor/archive/refs/tags/v_0.6.6.tar.gz',
+    download_url='https://github.com/denisduarte/workplace_extractor/archive/refs/tags/v_0.6.7.tar.gz',
     keywords=['extraction', 'posts', 'workplace'],
     install_requires=[
         'numpy>=1.20',
